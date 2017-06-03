f77531e714783ef942555c56d5cc11fdad067058
linhongwei.zqgame <360710445@qq.com>
2017-06-03 16:19:30 +0800

    Util 共用类

---
+++ Assets/Scripts/Core/Util.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Util {
+
+    /// <summary>
+    /// 检查对象渲染器是否在摄像机的可见范围内
+    /// </summary>
+    /// <param name="renderer">渲染对象</param>
+    /// <param name="camera">摄像机</param>
+    /// <returns></returns>
+    public static bool IsVisibleFrom(this Renderer renderer, Camera camera)
+    {
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
+        return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+    }
+}
