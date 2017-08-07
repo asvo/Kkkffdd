b5d741b443ff9e2e23c0ec9bbe0aba95f3f3b15e
linhongwei.zqgame <360710445@qq.com>
2017-08-07 20:42:57 +0800

    Util。cs添加深度拷贝方法

--- Assets/Scripts/Core/Util.cs
+++ Assets/Scripts/Core/Util.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System;
 
 public static class Util
 {
@@ -138,6 +140,29 @@ public static class Util
         string savePath = Application.persistentDataPath + "/" + path + ".json";
         bool bExists = System.IO.File.Exists(savePath);
         return bExists;
+    }
+
+    public static T DeepClone<T>(T source)
+    {
+        if (!typeof(T).IsSerializable)
+        {
+            throw new ArgumentException("The type must be serializable.", "source");
+        }
+
+        //don't serialize a null object,simple return the default for that object
+        if (object.ReferenceEquals(source, null))
+        {
+            return default(T);
+        }
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        Stream stream = new MemoryStream();
+        using (stream)
+        {
+            formatter.Serialize(stream, source);
+            stream.Seek(0, SeekOrigin.Begin);
+            return (T)formatter.Deserialize(stream);
+        }
     }
 
     public static void LogAsvo(string content)
