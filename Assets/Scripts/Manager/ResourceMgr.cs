4845fe774236542e64c77114446357965f9c5be6
asvo <asv_o@sina.com>
2017-09-06 22:29:09 +0800

    -场景背景滚动

---
+++ Assets/Scripts/Manager/ResourceMgr.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class ResourceMgr : Single<ResourceMgr> {
+
+	
+    public Sprite LoadSpriteByFile(string fileName)
+    {
+        FileStream fs = new FileStream(Application.dataPath + "/Arts/scene/" + fileName + ".png", FileMode.Open,
+            FileAccess.Read);
+        fs.Seek(0, SeekOrigin.Begin);
+
+        byte[] bytes = new byte[fs.Length];
+        fs.Read(bytes, 0, (int)fs.Length);
+        fs.Close();
+        fs.Dispose();
+        fs = null;
+
+        int wid = 4800, hei = 1080;
+        Texture2D t2d = new Texture2D(4800, 1080);
+        t2d.LoadImage(bytes);
+
+        Sprite sp = Sprite.Create(t2d, new Rect(0, 0, t2d.width, t2d.height), new Vector2(0.5f, 0.5f));
+
+        return sp;
+    }
+
+}
---
+++ Assets/Scripts/SceneCtrl.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneCtrl : MonoBehaviour {
+
+    private Transform mBgFore;
+    private Transform BgFore
+    {
+        get
+        {
+            if (null == mBgFore)
+            {
+                mBgFore = GameObject.Find("scenes/bg_fore").transform;
+            }
+            return mBgFore;
+        }
+    }
+    private Transform mBgMid;
+    private Transform BgMid
+    {
+        get
+        {
+            if (null == mBgMid)
+            {
+                mBgMid = GameObject.Find("scenes/bg_mid").transform;
+            }
+            return mBgMid;
+        }
+    }
+    private Transform mBgBackend;
+    private Transform BgBackend
+    {
+        get
+        {
+            if (null == mBgBackend)
+            {
+                mBgBackend = GameObject.Find("scenes/bg_backend").transform;
+            }
+            return mBgBackend;
+        }
+    }
+
+    void Start()
+    {
+        LoadScene();
+    }
+
+    private void LoadScene()
+    {
+        BgFore.GetComponent<SpriteRenderer>().sprite = ResourceMgr.Instance().LoadSpriteByFile("new_bg_1");
+        BgMid.GetComponent<SpriteRenderer>().sprite = ResourceMgr.Instance().LoadSpriteByFile("new_bg_2");
+        BgBackend.GetComponent<SpriteRenderer>().sprite = ResourceMgr.Instance().LoadSpriteByFile("new_bg_3");
+        mMidBgPos = BgMid.localPosition;
+        mBackBgPos = BgBackend.localPosition;
+    }
+
+    void Update()
+    {
+        LerpMoveBg();
+    }
+
+    private float PlayerMostLeft = -33f;
+    private float PlayerMostRight = 14f;
+    private float mDelta;
+
+    private float MidPosLeft = -1.5f;
+    private float MidPosRight = 10f;
+    private Vector3 mMidBgPos;
+
+    private float BackPosLeft = 0f;
+    private float BackPosRight = 15f;
+    private Vector3 mBackBgPos;
+
+    private void LerpMoveBg()
+    {
+        float curPlayerX = GameManager.Instance().MainPlayer.transform.position.x;
+        mDelta = PlayerMostRight - PlayerMostLeft;
+        float f = Mathf.Clamp01((curPlayerX - PlayerMostLeft)/mDelta);
+        mMidBgPos.x = Mathf.Lerp(MidPosLeft, MidPosRight, f);
+        BgMid.localPosition = mMidBgPos;
+        mBackBgPos.x = Mathf.Lerp(BackPosLeft, BackPosRight, f);
+        BgBackend.localPosition = mBackBgPos;
+    }
+}
